38e5c6f4bf18ceb45582a52d8e0df81866a18627
Craig Spaeth <craigspaeth@gmail.com>
2013-12-22 20:59:08 -0500

    latest

--- apps/commits/index.js
+++ apps/commits/index.js
@@ -1,15 +1,20 @@
 // 
-// The express app for the "commits" app.
-// 
-// Simply exports the express instance to be mounted into the project, 
-// and loads the routes.
+// The express app for the list of commits for an Artsy repo.
 // 
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express'),
+    Commits = require('../../collections/commits.js');
 
 var app = module.exports = express();
 
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'jade');
-app.get('/commits', routes.index);
+
+app.get('/commits', function(req, res, next) {
+  new Commits([], { owner: 'artsy', repo: 'flare' }).fetch({
+    success: function(commits) {
+      res.render('index', { commits: commits.models });
+    },
+    error: function(m, err) { next(err.text); }
+  });
+});
--- components/layout/client.js
+++ components/layout/client.js
@@ -18,5 +18,9 @@ $(function() {
 $(document).on('click', 'a', function(e) {
   e.preventDefault();
   var href = $(this).attr('href').replace(/^\//, '');
-  Backbone.history.navigate(href, { trigger: true });
+  // Add a little fade-in-fade-out action to clarify it's on the client
+  $('body').animate({ opacity: 0 }, 'fast', function() {
+    Backbone.history.navigate(href, { trigger: true });
+    $('body').animate({ opacity: 1 }, 'fast');
+  });
 });
